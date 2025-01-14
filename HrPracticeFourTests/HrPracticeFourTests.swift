e51bd6984fe5fd1032fb7f2b8667604a8c6b1b4c
Austin Harrison <austinh5544@gmail.com>
2025-01-13 16:01:18 -0800

    Initial Commit

---
+++ HrPracticeFourTests/HrPracticeFourTests.swift
@@ -0,0 +1,17 @@
+//
+//  HrPracticeFourTests.swift
+//  HrPracticeFourTests
+//
+//  Created by Austin Harrison on 1/13/25.
+//
+
+import Testing
+@testable import HrPracticeFour
+
+struct HrPracticeFourTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
